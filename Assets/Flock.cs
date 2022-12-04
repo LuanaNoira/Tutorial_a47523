d81197046590413659fbc5a28ad7b2aa768d924d
a47523 <a47523@alunos.ipb.pt>
2022-12-04 00:42:25 +0000

    Atividade 2 - 1. Flocking Part 3

--- Assets/Flock.cs
+++ Assets/Flock.cs
@@ -19,17 +19,26 @@ public class Flock : MonoBehaviour
     {
         Bounds b = new Bounds(myManager.transform.position, myManager.swimLimits*2);
 
+        RaycastHit hit = new RaycastHit();
+         Vector3 direction = Vector3.zero;
+
         if(!b.Contains(transform.position))
         {
             turning = true;
+            direction = myManager.transform.position - transform.position;
         }
+           else if (Physics.Raycast(transform.position, this.transform.forward * 50, out hit))
+            {
+                turning = true;
+                direction = Vector3.Reflect(this.transform.forward,hit.normal);
+            }
         else
         {
             turning = false;
         }
+        
         if(turning)
         {
-            Vector3 direction = myManager.transform.position - transform.position;
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), myManager.rotationSpeed * Time.deltaTime);
         }
         else
@@ -43,7 +52,7 @@ public class Flock : MonoBehaviour
                         ApplyRules();
                     }
         }
-        
+
         transform.Translate(0,0, Time.deltaTime * speed);
     }
 
